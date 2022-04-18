953472c42e9517c0cbd07c4f42eb25601ddbb1a9
Surya Krishna Moorthy <37917817+SuryaKrishnaMoorthy@users.noreply.github.com>
2022-04-18 16:21:24 -0400

    Add paginate

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import DropDown from "./DropDown";
 import Pagination from "./Pagination";
 import data from "./data";
@@ -8,13 +8,21 @@ export default function App() {
   const [currentPage, setCurrentpage] = useState(1);
   const [rowsPerPage, setRowsPerPage] = useState(5);
 
+  useEffect(() => {}, [rowsPerPage]);
+
   const handleDropDownChange = (e) => {
     setRowsPerPage(e.target.value);
   };
 
+  //Get current Rows
   const indexOfLastRow = currentPage * rowsPerPage;
   const indexOfFirstRow = indexOfLastRow - rowsPerPage;
   const currentData = data.slice(indexOfFirstRow, indexOfLastRow);
+
+  //Change Page
+  const paginate = (page) => {
+    setCurrentpage(page);
+  };
 
   return (
     <div className="App">
@@ -41,7 +49,11 @@ export default function App() {
           )}
         </tbody>
       </table>
-      <Pagination rowsPerPage={rowsPerPage} totalRows={data.length} />
+      <Pagination
+        rowsPerPage={rowsPerPage}
+        totalRows={data.length}
+        paginate={paginate}
+      />
     </div>
   );
 }
--- src/Pagination.js
+++ src/Pagination.js
@@ -1,14 +1,16 @@
-const Pagination = ({ rowsPerPage, totalRows }) => {
+const Pagination = ({ rowsPerPage, totalRows, paginate }) => {
   const numberOfPages = Math.ceil(totalRows / rowsPerPage);
   const pages = [];
   for (let i = 1; i <= numberOfPages; i++) {
     pages.push(i);
   }
-  console.log(pages);
+
   return (
     <ul>
       {pages.map((page) => (
-        <li key={page}>{page}</li>
+        <li key={page} onClick={() => paginate(page)}>
+          {page}
+        </li>
       ))}
     </ul>
   );
